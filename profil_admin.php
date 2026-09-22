dc4e552b577e9ced2d0562efe3b9b6f5679c179a
didismisbahudin17-design <didismisbahudin17@gmail.com>
2026-09-22 08:07:54 +0700

    Menambahkan fitur logout

---
+++ profil_admin.php
@@ -0,0 +1,46 @@
+<?php
+// Menghubungkan ke file koneksi database
+include "config/koneksi.php";
+?>
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Profil Admin - Portal Kurikulum</title>
+    
+    <!-- Fonts & Icons -->
+    <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@400;600;700;800&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+    
+    <!-- CSS External -->
+    <link rel="stylesheet" href="assets/style.css">
+</head>
+<body>
+
+
+
+    <div class="container">
+        
+        <!-- Kartu Utama Profil Admin -->
+        <div class="admin-card">
+            
+            <!-- Header Kartu & Photo Profil -->
+            <div class="admin-header">
+                <div class="admin-avatar-wrapper">
+                    <img src="">
+                </div>
+            </div>
+            <div class="admin-info">
+                <h2 class="admin-name">Didis Misbahudin</h2>
+                <p class="admin-class">XI RPL 1</p>
+            </div
+
+            
+
+    </div>
+
+    <!-- Script External -->
+    <script src="assets/script.js"></script>
+</body>
+</html>
